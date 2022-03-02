d5a1a95f0c30b7d3c1901d414e474d0d769c0012
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-03 00:11:43 +0100

    test(test_check_item_type_object): add test to verify custom item type object

---
+++ tests/domain/objectsTest.py
@@ -0,0 +1,14 @@
+from domain.normal_item import Normal_Item
+import pytest
+
+def test_check_item_type_object():
+
+    normal_item = Normal_Item("Elixir of the Mongoose", 5, 7)
+    assert isinstance(normal_item, Normal_Item)
+    assert 5 == normal_item.sell_in
+    assert 7 == normal_item.quality
+
+    normal_item.update_quality()
+
+    assert 4 == normal_item.sell_in
+    assert 6 == normal_item.quality
