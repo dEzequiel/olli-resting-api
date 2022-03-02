e14f6b39b42d6abba9a2d6d7a53e51503868259f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-02 01:51:00 +0100

    feat(model): innitial setup

---
+++ model/inventory.py
@@ -0,0 +1,13 @@
+import json
+
+myjson = {
+    "item1" : {
+        "advanced_targeting_enabled": "f",
+        "category_name": "E-commerce/ Shopping",
+        "click_lifespan": "168",
+        "conversion_cap": "50",
+        "currency": "USD",
+        "default_payout": "1.5"
+    }
+
+}
