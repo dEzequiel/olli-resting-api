f89868897742b0aa4f8ee7e78e63631aff9580fd
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-04 00:24:49 +0100

    test(inventoryTest): clean inventory list before adding new ones

--- tests/model/inventoryTest.py
+++ tests/model/inventoryTest.py
@@ -1,18 +1,16 @@
 from operator import inv
 import pytest
 from domain.normal_item import Normal_Item
-from repository.inventory import Inventory
+from repository import inventory
 
 def test_object_into_dict():
 
-    inventory = Inventory()
     normal_item = Normal_Item("Elixir of the Mongoose", 5, 7)
 
     inventory.new_item(normal_item)
     assert {"name": "Elixir of the Mongoose", "sell_in": 5, "quality": 7} == inventory.inventoryItems[0]
 
 def test_object_class():
-    inventory = Inventory()
     normal_item = Normal_Item("Elixir of the Mongoose", 5, 7)
 
     inventory.new_item(normal_item)
@@ -20,13 +18,14 @@ def test_object_class():
 
 def test_get_object_by_name():
 
-    inventory = Inventory()
+    inventory.clean_inventory_list()
+
     normal_item = Normal_Item("Elixir of the Mongoose", 5, 7)
     inventory.new_item(normal_item)
     normal_item2 = Normal_Item("Elixir of the Mongoose", 1, 2)
     inventory.new_item(normal_item2)
 
-    assert 2 == len(inventory.inventoryItems)
+    assert 2 == inventory.get_inventory_size()
     expected_result = [{'name': 'Elixir of the Mongoose', 'quality': 7, 'sell_in': 5}]
 
     assert expected_result == inventory.get_item('Elixir of the Mongoose')
