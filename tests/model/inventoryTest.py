7035755e36ceed8697504224f28b68bbe5540f11
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-03 13:28:35 +0100

    test(test_get_object_by_name): add method to return the inventory object by name

--- tests/model/inventoryTest.py
+++ tests/model/inventoryTest.py
@@ -17,3 +17,16 @@ def test_object_class():
 
     inventory.new_item(normal_item)
     assert isinstance (inventory.inventoryItems[0], dict)
+
+def test_get_object_by_name():
+
+    inventory = Inventory()
+    normal_item = Normal_Item("Elixir of the Mongoose", 5, 7)
+    inventory.new_item(normal_item)
+    normal_item2 = Normal_Item("Elixir of the Mongoose", 1, 2)
+    inventory.new_item(normal_item2)
+
+    assert 2 == len(inventory.inventoryItems)
+    expected_result = [{'name': 'Elixir of the Mongoose', 'quality': 7, 'sell_in': 5}]
+    
+    assert expected_result == inventory.get_item('Elixir of the Mongoose')
