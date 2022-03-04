5ea00e8649349e7cadce83d5249c40ee66ed0692
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-04 16:28:33 +0100

    fix(get_item): allow the method to return multiples items

--- repository/inventory.py
+++ repository/inventory.py
@@ -1,33 +1,33 @@
-from domain.stock_item import Stock_Item
-
 class Inventory:
 
-    inventoryItems = []
-    inventoryDemandItems = []
+    inventory_items = [
+        {'name': 'Elixir of the Mongoose', 'sell_in': 5, 'quality': 7}
+        ]
 
     @classmethod
     def new_item(cls, item):
-        cls.inventoryItems.append(item.__dict__)
+        cls.inventory_items.append(item.__dict__)
 
     @classmethod
     def get_item(cls, name):
-        for item in cls.inventoryItems:
+        items = []
+        for item in cls.inventory_items:
             if item['name'] == name:
-                cls.inventoryDemandItems.append(item)
-            else:
-                return {name:"not found"}
+                items.append(item)
 
-        return cls.inventoryDemandItems
+        if len(items) != 0:
+            return items
+        else:
+            return {name:"not found"}
 
     @classmethod
     def get_inventory(cls):
-        return cls.inventoryItems
+        return cls.inventory_items
 
     @classmethod
     def get_inventory_size(cls):
-        return len(cls.inventoryItems)
+        return len(cls.inventory_items)
 
     @classmethod
     def clean_inventory_list(cls):
-        cls.inventoryItems.clear()
-        cls.inventoryDemandItems.clear()
+        cls.inventory_items.clear()
