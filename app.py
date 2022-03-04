0ed01ab3afa7858aa0800ac629c6b095d7a25dfd
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-04 17:00:39 +0100

    feat: main flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from controller.item import Item
+
+app = Flask(__name__)
+
+# API REST
+api = Api(app, catch_all_404s=True)
+
+api.add_resource(Item, '/item/<name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
