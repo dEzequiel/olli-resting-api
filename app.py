6522aa3177000f0d3a0836a2a481aca5c257873c
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-06 23:32:23 +0100

    feat: add new resource aiming to the Root route

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from controller.item import Item
+from controller.root import Root
 
 app = Flask(__name__)
 
 # API REST
 api = Api(app, catch_all_404s=True)
 
+api.add_resource(Root, "/")
 api.add_resource(Item, "/item/<name>")
 
 if __name__ == "__main__":
