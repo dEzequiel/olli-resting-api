b2a3a5dba97966326c481e704357eb53d27632a1
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-07 19:27:07 +0100

    feat(app): add new entrypoints

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import sqlite3
 from flask import Flask
 from flask_restful import Resource, Api
 from controller.item import Item
+from controller.items import Items
+from controller.items_by_sell_in import ItemsBySellin
 from controller.root import Root
+from repository.get_all_items import get_all_items
+from repository.get_item import get_item
+
 
 app = Flask(__name__)
 
@@ -9,7 +15,9 @@ app = Flask(__name__)
 api = Api(app, catch_all_404s=True)
 
 api.add_resource(Root, "/")
-api.add_resource(Item, "/item/<name>")
+api.add_resource(Items, "/items")
+api.add_resource(Item, "/item/name/<name>")
+api.add_resource(ItemsBySellin, "/item/sellin/<sell_in>")
 
 if __name__ == "__main__":
     app.run(debug=True)
