a90bcca16f154a8d88af3bb661d40a3ec9fc7712
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-07 19:26:07 +0100

    feat(Services): call repository methods, add layer

--- services/service.py
+++ services/service.py
@@ -1,9 +1,17 @@
 from socket import inet_ntoa
-from repository.inventory import Inventory
-
+from repository.get_all_items import get_all_items
+from repository.get_item import get_item
+from repository.get_by_sell_in import get_by_sell_in
 
 class Services:
     @staticmethod
-    def get_object(name):
-        item = Inventory.get_item(name)
-        return item
+    def get_items():
+        return get_all_items()
+
+    @staticmethod
+    def get_item(name):
+        return get_item(name)
+
+    @staticmethod
+    def get_item_by_sell_in(sell_in):
+        return get_by_sell_in(sell_in)
