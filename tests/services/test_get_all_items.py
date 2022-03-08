93c183740a87ebaeef1baa59489af25352174223
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-08 02:13:02 +0100

    test(test_get_all_items): add integration test to check if the data returned by service layer is the same as data access layer

---
+++ tests/services/test_get_all_items.py
@@ -0,0 +1,16 @@
+from services.service import Services
+from repository.get_item import get_all_items
+from app import app
+import pytest
+
+services = Services()
+
+def test_get_all_items():
+    with app.app_context():
+
+        # item make reference to the object returned by services layer
+        # expected_result make reference to the object returned by data access layer
+
+        item = services.get_items()
+        expected_result = get_all_items()
+        assert expected_result == item
