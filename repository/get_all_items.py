92b07cd89cf37fffe0fd4923a27262b832d193d2
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-08 17:59:13 +0100

    feat(get_all_items): use of list comprehension and close the convention

--- repository/get_all_items.py
+++ repository/get_all_items.py
@@ -4,13 +4,6 @@ from repository.database import get_db
 
 def get_all_items():
     database = get_db()
-    database.row_factory = sqlite3.Row
-    cursor = database.cursor()
-
-    r = cursor.execute('SELECT * FROM inventory')
-
-    items = []
-    for row in r.fetchall():
-        items.append(dict(row))
-
-    return items
+    items = [dict(row) for row in database.execute('SELECT * FROM inventory').fetchall()]
+    database.close()
+    return items if len(items) != 0 else "Inventory is empty"
