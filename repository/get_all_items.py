820bd8d3948ed4d09e633cebdb36ff2f8ea523df
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-08 18:02:22 +0100

    refactor(get_all_items): delete unnecesary method

--- repository/get_all_items.py
+++ repository/get_all_items.py
@@ -4,6 +4,6 @@ from repository.database import get_db
 
 def get_all_items():
     database = get_db()
-    items = [dict(row) for row in database.execute('SELECT * FROM inventory').fetchall()]
+    items = [dict(row) for row in database.execute('SELECT * FROM inventory')]
     database.close()
     return items if len(items) != 0 else "Inventory is empty"
