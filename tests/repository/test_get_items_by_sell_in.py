edf78ee3a31f74c7afc5fad8a5d6abfe07e1eff1
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-08 14:35:43 +0100

    test(test_no_items_with_sell_in_found): add test to check if the response when no items found is correct

--- tests/repository/test_get_items_by_sell_in.py
+++ tests/repository/test_get_items_by_sell_in.py
@@ -7,3 +7,8 @@ def test_get_items_by_sell_in():
         expected_result = [{"name": "Sulfuras", "sell_in": 1, "quality": 1},
                             {'name': 'Elixir of the Mongoose', 'quality': 1, 'sell_in': 1}]
         assert expected_result == get_item_by_sell_in(1)
+
+def  test_no_items_with_sell_in_found():
+    with app.app_context():
+        expected_result = "Items with sell_in=100 not found"
+        assert expected_result == get_item_by_sell_in(100)
