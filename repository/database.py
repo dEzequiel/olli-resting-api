56c490306655d409d6d3666c6c20660d3b2e2624
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-08 18:00:08 +0100

    feat(get_db): convert db row factory into Row object

--- repository/database.py
+++ repository/database.py
@@ -7,9 +7,12 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g.__database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
     return db
 
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+#tierdown cerrar conexion
