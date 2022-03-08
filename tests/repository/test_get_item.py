2a4a33ed584065a336af4c9fd005a39213379002
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-08 01:57:21 +0100

    test(test_get_multiple_item): add test to return multiple items by name

--- tests/repository/test_get_item.py
+++ tests/repository/test_get_item.py
@@ -6,3 +6,9 @@ def test_get_one_item():
     with app.app_context():
         expected_result = [{"name": "Sulfuras", "sell_in": 1, "quality": 1}]
         assert expected_result == get_item("Sulfuras")
+
+def test_get_multiple_item():
+    with app.app_context():
+        expected_result = [{"name": "Elixir of the Mongoose", "sell_in": 7, "quality": 5},
+                        {"name": "Elixir of the Mongoose", "sell_in": 1, "quality": 1}]
+        assert expected_result == get_item("Elixir of the Mongoose")
