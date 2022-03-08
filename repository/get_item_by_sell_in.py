0a69a7a012fca3628a65ec82d044e71fe455b4ea
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-08 17:42:22 +0100

    feat(get_item_by_sell_in): add method to return list of items with a specific sell in

--- repository/get_item_by_sell_in.py
+++ repository/get_item_by_sell_in.py
@@ -1,6 +1,17 @@
+import sqlite3
+from repository.database import get_db
 from repository.get_all_items import get_all_items
 
 
-def get_item_by_sell_in(sell_in):
-    items = list(filter(lambda item: item['sell_in'] == sell_in, get_all_items()))
-    return items if len(items) != 0 else f"Items with sell_in={sell_in} not found"
+def get_item_by_sell_in(sellin):
+    database = get_db()
+    database.row_factory = sqlite3.Row
+    cursor = database.cursor()
+
+    r = cursor.execute(f'SELECT * from inventory WHERE sell_in={sellin}')
+
+    items = []
+    for row in r.fetchall():
+        items.append(dict(row))
+
+    return items
