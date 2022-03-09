cd317ecbfcd141ddca73dc9c5ad0d9a4e7e883e8
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-09 19:05:24 +0100

    refactor(get_item_by_sell_in): use of list comprehensionm

--- repository/get_item_by_sell_in.py
+++ repository/get_item_by_sell_in.py
@@ -5,13 +5,6 @@ from repository.get_all_items import get_all_items
 
 def get_item_by_sell_in(sellin):
     database = get_db()
-    database.row_factory = sqlite3.Row
-    cursor = database.cursor()
-
-    r = cursor.execute(f'SELECT * from inventory WHERE sell_in={sellin}')
-
-    items = []
-    for row in r.fetchall():
-        items.append(dict(row))
-
-    return items
+    item = [dict(row) for row in database.execute(f"SELECT * FROM inventory WHERE sell_in='{sellin}'")]
+    database.close()
+    return item if len(item) != 0 else {f"Sell in {sellin}":"not found"}
