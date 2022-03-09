da376e3591d0358f4d8ec95e89129cd7f7cbe2e4
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-09 17:23:21 +0100

    refactor(get_item): use of list comprehension, sql query format and close db

--- repository/get_item.py
+++ repository/get_item.py
@@ -1,9 +1,8 @@
-import sqlite3
 from repository.database import get_db
 from repository.get_all_items import get_all_items
 
 def get_item(name):
-
-    items = list(filter(lambda item: item['name'] == name, get_all_items()))
-
-    return items if len(items) != 0 else {name:"not found"}
+    database = get_db()
+    item = [dict(row) for row in database.execute(f"SELECT * FROM inventory WHERE name='{name}'")]
+    database.close()
+    return item if len(item) != 0 else {name:"not found"}
