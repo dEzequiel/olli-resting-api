911e4da12b6b06da781d03ce4c0060455299e363
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-11 00:52:41 +0100

    feat(add_new_item): add method to post a new item into db via services

--- services/service.py
+++ services/service.py
@@ -3,6 +3,7 @@ from repository.get_all_items import get_all_items
 from repository.get_item import get_item
 from repository.get_item_by_sell_in import get_item_by_sell_in
 from repository.get_item_by_quality import get_item_by_quality
+from repository.add_item import add_item
 
 
 class Services:
@@ -21,3 +22,7 @@ class Services:
     @staticmethod
     def get_item_by_quality(quality):
         return get_item_by_quality(quality)
+
+    @staticmethod
+    def add_new_item(name):
+        add_item(name)
