9cd2612e19b1a53254f6fdcedf7cef9f2c625484
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-11 00:51:59 +0100

    feat(add_item): add item via post

--- repository/add_item.py
+++ repository/add_item.py
@@ -1,9 +1,7 @@
 from repository.database import get_db
 
-def add_item(name, sell_in, quality):
+def add_item(name, sell_in=10, quality=10):
     database = get_db()
-
-    item = [dict(row) for row in database.execute("INSERT INTO inventory (name, sell_in, quality) VALUES (?, ?, ?)", (name, sell_in, quality))]
-
+    database.execute("INSERT INTO inventory (name, sell_in, quality) VALUES (?, ?, ?)", (name, sell_in, quality))
     database.commit()
-    return item
+    database.close()
