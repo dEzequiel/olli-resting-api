8da8dd4a2c163ce1b35e8e74c7c888a3fe7c4726
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-14 16:33:16 +0100

    refactor: black formatted

--- repository/add_item.py
+++ repository/add_item.py
@@ -1,9 +1,15 @@
 from repository.database import get_db
 
+
 def add_item(name, sell_in, quality):
     database = get_db()
 
-    item = [dict(row) for row in database.execute("INSERT INTO inventory VALUES (?, ?, ?)", (name, sell_in, quality))]
+    item = [
+        dict(row)
+        for row in database.execute(
+            "INSERT INTO inventory VALUES (?, ?, ?)", (name, sell_in, quality)
+        )
+    ]
 
     database.commit()
     database.close()
--- tests/repository/test_add_item.py
+++ tests/repository/test_add_item.py
@@ -5,13 +5,18 @@ from repository.database import get_db
 from repository.add_item import add_item
 import sqlite3
 
+
 def test_add_new_item():
     with app.app_context():
-        add_item('Rocky', 1, 1)
+        add_item("Rocky", 1, 1)
 
         database = get_db()
-        item = [dict(row) for row in database.execute("SELECT * FROM inventory ORDER BY quality DESC LIMIT 1;")]
+        item = [
+            dict(row)
+            for row in database.execute(
+                "SELECT * FROM inventory ORDER BY quality DESC LIMIT 1;"
+            )
+        ]
 
-
-        expected_result = [{'name':'Rocky', 'sell_in':1, 'quality':1}]
-        assert item ==expected_result
+        expected_result = [{"name": "Rocky", "sell_in": 1, "quality": 1}]
+        assert item == expected_result
