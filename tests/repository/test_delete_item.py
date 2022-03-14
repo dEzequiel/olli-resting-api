ec3f7b69c62216b19dff1ae1bd9ea2654624ac43
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-15 00:11:41 +0100

    test(delete_item)

--- tests/repository/test_delete_item.py
+++ tests/repository/test_delete_item.py
@@ -2,10 +2,11 @@ import pytest
 from app import app
 from repository.database import get_db
 from repository.delete_item import delete_item
-import sqlite3
 
 def test_delete_item():
     with app.app_context():
+        assert "Item with id=3 was deleted" == delete_item(3)
 
-        database = get_db()
-        assert [{"id": 3, "name": "Elixir of the Mongoose", "sell_in": 1, "quality": 1}] == delete_item(3)
+def test_delete_item_not_found():
+    with app.app_context():
+        assert "Item with id=5 was not found" == delete_item(5)
