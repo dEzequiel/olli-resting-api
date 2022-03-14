a954d1f1c1c568b62190d722d347fcce252662ab
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-14 19:08:51 +0100

    refactor: add db id field

--- tests/repository/test_get_item.py
+++ tests/repository/test_get_item.py
@@ -5,15 +5,15 @@ from app import app
 
 def test_get_one_item():
     with app.app_context():
-        expected_result = [{"name": "Sulfuras", "sell_in": 1, "quality": 1}]
+        expected_result = [{"id": 1, "name": "Sulfuras", "sell_in": 1, "quality": 1}]
         assert expected_result == get_item("Sulfuras")
 
 
 def test_get_multiple_item():
     with app.app_context():
         expected_result = [
-            {"name": "Elixir of the Mongoose", "sell_in": 7, "quality": 5},
-            {"name": "Elixir of the Mongoose", "sell_in": 1, "quality": 1},
+            {"id": 2, "name": "Elixir of the Mongoose", "sell_in": 7, "quality": 5},
+            {"id": 3, "name": "Elixir of the Mongoose", "sell_in": 1, "quality": 1},
         ]
         assert expected_result == get_item("Elixir of the Mongoose")
 
