4e5228d8f000db22d01f64b9f7e0392e52b1065d
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-14 16:19:30 +0100

    feat(Item): add http post method to item controller

--- controller/item.py
+++ controller/item.py
@@ -5,3 +5,6 @@ from services.service import Services
 class Item(Resource):
     def get(self, name):
         return Services.get_item(name), 200
+
+    def post(self, name):
+        return Services.post_item(name), 201
