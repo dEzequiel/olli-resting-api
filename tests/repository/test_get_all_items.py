a9e42467b3cfbfca3a260ef084da25011b7d05cf
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-14 19:03:10 +0100

    refactor: add id field

--- tests/repository/test_get_all_items.py
+++ tests/repository/test_get_all_items.py
@@ -8,26 +8,8 @@ from app import app
 def test_get_all_items():
     with app.app_context():
         expected_result = [
-            {"name": "Sulfuras", "sell_in": 1, "quality": 1},
-            {"name": "Elixir of the Mongoose", "sell_in": 7, "quality": 5},
-            {"name": "Elixir of the Mongoose", "sell_in": 1, "quality": 1},
-            {"name": "+5 Dextery Vest", "sell_in": 10, "quality": 20},
-            {"name": "Aged Brie", "sell_in": 2, "quality": 0},
-            {
-                "name": "Backstage passes to a TAFKAL80ETC concert",
-                "sell_in": 15,
-                "quality": 20,
-            },
-            {
-                "name": "Backstage passes to a TAFKAL80ETC concert",
-                "sell_in": 10,
-                "quality": 40,
-            },
-            {
-                "name": "Backstage passes to a TAFKAL80ETC concert",
-                "sell_in": 5,
-                "quality": 49,
-            },
+            {"id":1, "name": "Sulfuras", "sell_in": 1, "quality": 1},
+            {"id":2,"name": "Elixir of the Mongoose", "sell_in": 7, "quality": 5},
         ]
 
         assert expected_result == get_all_items()
