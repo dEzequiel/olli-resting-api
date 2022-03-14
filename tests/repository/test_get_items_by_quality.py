1bec1adb0aea815fd7dadc4014bf9b49cd5a756a
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-14 19:10:22 +0100

    refactor: add id field

--- tests/repository/test_get_items_by_quality.py
+++ tests/repository/test_get_items_by_quality.py
@@ -6,7 +6,7 @@ from repository.get_item_by_quality import get_item_by_quality
 def test_get_item_by_quality():
     with app.app_context():
         expected_result = [
-            {"name": "Elixir of the Mongoose", "quality": 5, "sell_in": 7}
+            {"id": 2, "name": "Elixir of the Mongoose", "quality": 5, "sell_in": 7}
         ]
         real_result = get_item_by_quality(5)
         assert expected_result == real_result
--- tests/repository/test_get_items_by_sell_in.py
+++ tests/repository/test_get_items_by_sell_in.py
@@ -6,8 +6,8 @@ from app import app
 def test_get_items_by_sell_in():
     with app.app_context():
         expected_result = [
-            {"name": "Sulfuras", "sell_in": 1, "quality": 1},
-            {"name": "Elixir of the Mongoose", "quality": 1, "sell_in": 1},
+            {"id":1, "name": "Sulfuras", "sell_in": 1, "quality": 1},
+            {"id": 3, "name": "Elixir of the Mongoose", "quality": 1, "sell_in": 1},
         ]
         assert expected_result == get_item_by_sell_in(1)
 
