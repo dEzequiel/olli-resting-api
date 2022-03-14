e8f487fedea2f2be4f21f8418ddd3ccfa5d2a5d2
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-03-15 00:11:25 +0100

    test(add_new_item)

--- tests/repository/test_add_item.py
+++ tests/repository/test_add_item.py
@@ -1,22 +1,20 @@
-# from unittest import result
-# import pytest
-# from app import app
-# from repository.database import get_db
-# from repository.add_item import add_item
-# import sqlite3
+import pytest
+from app import app
+from repository.database import get_db
+from repository.add_item import add_item
 
 
-# def test_add_new_item():
-#     with app.app_context():
-#         add_item("Rocky", 1, 1)
+def test_add_new_item():
+    with app.app_context():
+        add_item(None, "Elixir of the Mongoose", 10, 10)
 
-#         database = get_db()
-#         item = [
-#             dict(row)
-#             for row in database.execute(
-#                 "SELECT * FROM inventory ORDER BY quality DESC LIMIT 1;"
-#             )
-#         ]
+        database = get_db()
+        item = [
+            dict(row)
+            for row in database.execute(
+                "SELECT * FROM inventory ORDER BY quality DESC LIMIT 1;"
+            )
+        ]
 
-#         expected_result = [{"name": "Rocky", "sell_in": 1, "quality": 1}]
-#         assert item == expected_result
+        expected_result = [{"id":16, "name": "Elixir of the Mongoose", "sell_in": 10, "quality": 10}]
+        assert item == expected_result
