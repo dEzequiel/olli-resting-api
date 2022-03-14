ce258c9bc04f173fb69d5f94cb43fe25ef55d1f1
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-14 16:17:57 +0100

    refactor(SQL QUERY): make query shorter

--- repository/add_item.py
+++ repository/add_item.py
@@ -3,7 +3,7 @@ from repository.database import get_db
 def add_item(name, sell_in, quality):
     database = get_db()
 
-    item = [dict(row) for row in database.execute("INSERT INTO inventory (name, sell_in, quality) VALUES (?, ?, ?)", (name, sell_in, quality))]
+    item = [dict(row) for row in database.execute("INSERT INTO inventory VALUES (?, ?, ?)", (name, sell_in, quality))]
 
     database.commit()
     return item
