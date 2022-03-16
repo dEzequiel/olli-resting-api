a0adb42671ebd4085f5a19b1c95a375bd43050e1
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-16 16:48:19 +0100

    feat: add method and command to insert items into db

--- repository/commands.py
+++ repository/commands.py
@@ -14,3 +14,14 @@ def init_db():
 def init_db_command():
     init_db()
     click.echo("Database created")
+
+def insert_db():
+    database = get_db()
+
+    with current_app.open_resource('repository/inserts.sql') as f:
+        database.executescript(f.read().decode('utf8'))
+
+@database_command.cli.command('insert-db')
+def insert_db_command():
+    insert_db()
+    click.echo("Fields added to database")
