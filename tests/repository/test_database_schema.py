cbf62733b3ec7bcfc7a8eb44b691e48963e462db
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-18 20:03:38 +0100

    test: add test to validate database schema

--- tests/repository/test_database_schema.py
+++ tests/repository/test_database_schema.py
@@ -1,5 +1,18 @@
 import pytest
+import json
 from repository.inventory_test.database_test import get_db
+from repository.commands import init_db, insert_db
 
-def test_database_schema():
-    
+
+def test_database_schema(client, app):
+    with app.app_context():
+        init_db()
+        insert_db()
+
+    response = client.get("/items")
+    data = json.loads(response.get_data(as_text=True))
+
+    expected_keys = ['id', 'name', 'sell_in', 'quality']
+    schema_keys = list(data[0].keys())
+
+    assert expected_keys == schema_keys
