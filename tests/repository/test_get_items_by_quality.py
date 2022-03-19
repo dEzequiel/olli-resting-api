b552e13e0611eef881d52ce8e1e6f2371bf5ea68
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-03-19 18:21:25 +0100

    test & refactor: use of response methods ands json convertion

--- tests/repository/test_get_items_by_quality.py
+++ tests/repository/test_get_items_by_quality.py
@@ -1,19 +1,35 @@
 import pytest
-from app import app
+import json
+from repository.commands import init_db, insert_db
 from repository.get_item_by_quality import get_item_by_quality
 
 
-def test_get_item_by_quality():
+def test_get_item_by_quality(client, app):
     with app.app_context():
-        expected_result = [
-            {"id": 2, "name": "Elixir of the Mongoose", "quality": 5, "sell_in": 7}
-        ]
-        real_result = get_item_by_quality(5)
-        assert expected_result == real_result
+        init_db()
+        insert_db()
 
+    response = client.get("/items/quality/80")
+    data = json.loads(response.get_data(as_text=True))
 
-def test_no_item():
+    assert data[0]["id"] == 4
+    assert data[0]["name"] == "Sulfuras, Hand of Ragnaros"
+    assert data[0]["sell_in"] == 0
+    assert data[0]["quality"] == 80
+
+    assert data[1]["id"] == 5
+    assert data[1]["name"] == "Sulfuras, Hand of Ragnaros"
+    assert data[1]["sell_in"] == -1
+    assert data[1]["quality"] == 80
+
+    assert response.status_code == 200
+
+def test_no_item(client, app):
     with app.app_context():
-        expected_result = {"Quality 1000": "not found"}
-        real_result = get_item_by_quality(1000)
-        assert expected_result == real_result
+        init_db()
+        insert_db()
+
+    response = client.get("/items/quality/500")
+    data = json.loads(response.get_data(as_text=True))
+
+    assert {"Quality 500": "not found"} == data
