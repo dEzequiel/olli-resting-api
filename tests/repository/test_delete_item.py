c3cd48fec98dee71b934f324dfc52c94f5b197a7
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-03-19 20:41:25 +0100

    test: add integration tests for delete item

--- tests/repository/test_delete_item.py
+++ tests/repository/test_delete_item.py
@@ -16,3 +16,25 @@ def test_unit_delete_item_not_found(app):
         init_db()
         insert_db()
         assert "Item with id=100 was not found" == delete_item(100)
+
+def test_delete_item(client, app):
+    with app.app_context():
+        init_db()
+        insert_db()
+
+    response = client.get("/delete/id/3")
+    data = response.get_data(as_text=True)
+
+    assert "Item with id=3 was deleted" == data
+    assert response.status_code == 200
+
+def test_delete_no_found_item(client, app):
+    with app.app_context():
+        init_db()
+        insert_db()
+
+    response = client.get("/delete/id/100")
+    data = response.get_data(as_text=True)
+
+    assert "Item with id=100 was not found" == data
+    assert response.status_code == 200
