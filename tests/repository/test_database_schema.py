2884a2949c776db1b94462fd02cc4c4fcc80f0e9
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-03-19 18:14:37 +0100

    no-relevant: delete unnecesary behaviours of db

--- tests/repository/test_database_schema.py
+++ tests/repository/test_database_schema.py
@@ -1,13 +1,9 @@
 import pytest
 import json
-from repository.inventory_test.database_test import get_db
 from repository.commands import init_db, insert_db
 
 
-def test_database_schema(client, app):
-    with app.app_context():
-        init_db()
-        insert_db()
+def test_database_schema(client):
 
     response = client.get("/items")
     data = json.loads(response.get_data(as_text=True))
