bf6e6f6f0db0ecb989c8e40e2999b5e75594438b
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-19 17:15:52 +0100

    feat & refactor: use of factory pattern offer by Flask to create app and use in test

--- app.py
+++ app.py
@@ -10,22 +10,25 @@ from controller.items_by_sell_in import ItemsBySellin
 from repository.commands import database_command
 from repository.inventory_test.database_test import database_test_command
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# API REST
-api = Api(app, catch_all_404s=True)
+    # API REST
+    api = Api(app, catch_all_404s=True)
 
-# Flask Blueprints encapsulate functionality, such as views, templates, and other resources.
-# A blueprint is a template for generating a "section" of a web application.
-app.register_blueprint(database_command)
-app.register_blueprint(database_test_command)
+    # Flask Blueprints encapsulate functionality, such as views, templates, and other resources.
+    # A blueprint is a template for generating a "section" of a web application.
+    app.register_blueprint(database_command)
+    app.register_blueprint(database_test_command)
 
 
-api.add_resource(Root, "/")
-api.add_resource(Items, "/items")
-api.add_resource(Item, "/item/name/<name>")
-api.add_resource(ItemsBySellin, "/items/sellin/<sellin>")
-api.add_resource(ItemsByQuality, "/items/quality/<quality>")
+    api.add_resource(Root, "/")
+    api.add_resource(Items, "/items")
+    api.add_resource(Item, "/item/name/<name>")
+    api.add_resource(ItemsBySellin, "/items/sellin/<sellin>")
+    api.add_resource(ItemsByQuality, "/items/quality/<quality>")
+
+    return app
 
 if __name__ == "__main__":
     app.run(debug=True)
