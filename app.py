21564b022dbfd075bc27e6e2c60fe764cc74938b
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-03-21 02:07:18 +0100

    feat: add new resource for methods that modify database

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from crypt import methods
 import sqlite3
 from flask import Flask
 from flask_restful import Resource, Api
 from controller.add_item import AddItem
 from controller.item import Item
+from controller.item_management import ItemManagement
 from controller.items import Items
 from controller.items_by_quality import ItemsByQuality
 from controller.root import Root
@@ -23,10 +25,14 @@ def create_app():
 
 
     api.add_resource(Root, "/")
+
     api.add_resource(Items, "/items")
-    api.add_resource(Item, "/item/name/<name>")
-    api.add_resource(ItemsBySellin, "/items/sellin/<sellin>")
-    api.add_resource(ItemsByQuality, "/items/quality/<quality>")
+    api.add_resource(Item, "/item/search/<name>")
+
+    api.add_resource(ItemsBySellin, "/item/sellin/<sellin>")
+    api.add_resource(ItemsByQuality, "/item/quality/<quality>")
+
+    api.add_resource(ItemManagement, "/item/del/<item_id>")
 
     return app
 
