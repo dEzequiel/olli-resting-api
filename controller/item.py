36db91e9ed682858f6eb8476347c6d304016b809
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-22 19:48:58 +0100

    feat: add delete method to the Item Resource

--- controller/item.py
+++ controller/item.py
@@ -8,3 +8,11 @@ class Item(Resource):
 
     def post(self, name):
         return Services.post_item(name), 201
+
+    # 204: The server has successfully fulfilled the request and that there is
+    # no additional content to send in the response payload body.
+    def delete(self, name):
+        if Services.delete_item(name):
+            return {f"{name}": "deleted"}, 200
+        else:
+            return {f"{name}": "not found"}, 404
