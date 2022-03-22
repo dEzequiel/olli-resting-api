8770a2dfb40690d84e0c1c3d04c0ad608be30a24
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-03-22 12:02:52 +0100

    test: fix logic

--- tests/repository/test_delete_item.py
+++ tests/repository/test_delete_item.py
@@ -1,6 +1,5 @@
 import pytest
 import json
-from repository.database import get_db
 from repository.inventory_test.database_test import delete_item
 from repository.inventory_test.database_test import init_db, insert_db
 
@@ -18,17 +17,14 @@ def test_unit_delete_item_not_found(app):
         insert_db()
         assert {"ID 100":"not found"} == delete_item(100)
 
+
 def test_delete_item(client, app):
     with app.app_context():
         init_db()
         insert_db()
 
     response = client.delete("/item/del/3")
-    # data = json.loads(response.get_data(as_text=True))
-
-    # assert {"ID 3":"deleted"} == data
-    # print(data)
-    assert response.status_code == 204
+    assert response.status_code == 200
 
 # def test_delete_no_found_item(client, app):
 #     with app.app_context():
@@ -36,7 +32,5 @@ def test_delete_item(client, app):
 #         insert_db()
 
 #     response = client.delete("/item/del/100")
-#     data = json.loads(response.get_data(as_text=True))
 
-#     assert {"ID 100":"not found"} == data
-#     assert response.status_code == 204
+#     assert response.status_code == 404
