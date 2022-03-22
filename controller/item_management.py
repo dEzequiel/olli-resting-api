82003caa6dbe0e81ef535525cc6306e9371cd294
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-03-22 12:04:04 +0100

    no-relevant: comment

--- controller/item_management.py
+++ controller/item_management.py
@@ -2,5 +2,8 @@ from flask_restful import Resource
 from services.service import Services
 
 class ItemManagement(Resource):
+
+    # 204: The server has successfully fulfilled the request and that there is
+    # no additional content to send in the response payload body.
     def delete(self, item_id):
-        return Services.delete_item(item_id), 204
+        return 200 if Services.delete_item(item_id) else 404
