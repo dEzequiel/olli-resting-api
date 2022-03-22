6233175c5af1973ae7e3743ec972a9d5c43d6855
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-22 16:42:09 +0100

    test: finish integration tests

--- tests/repository/test_delete_item.py
+++ tests/repository/test_delete_item.py
@@ -2,35 +2,43 @@ import pytest
 import json
 from repository.inventory_test.database_test import delete_item
 from repository.inventory_test.database_test import init_db, insert_db
+from repository import commands
 
 def test_unit_delete_item(app):
     with app.app_context():
         init_db()
         insert_db()
-
-        assert {"ID 3":"deleted"} == delete_item(3)
+        assert True == delete_item(3)
 
 
 def test_unit_delete_item_not_found(app):
     with app.app_context():
         init_db()
         insert_db()
-        assert {"ID 100":"not found"} == delete_item(100)
+        assert False == delete_item(100)
 
+
+# This tests uses production database.
+# Why? because we doing integration test by making a request to the app route /item/del/<item_id>
 
 def test_delete_item(client, app):
     with app.app_context():
-        init_db()
-        insert_db()
+        commands.init_db()
+        commands.insert_db()
 
     response = client.delete("/item/del/3")
+    data = json.loads(response.get_data(as_text=True))
+
+    assert {"ID 3": "deleted"} == data
     assert response.status_code == 200
 
 def test_delete_no_found_item(client, app):
     with app.app_context():
-        init_db()
-        insert_db()
+        commands.init_db()
+        commands.insert_db()
 
     response = client.delete("/item/del/100")
+    data = json.loads(response.get_data(as_text=True))
 
+    assert {"ID 100": "not found"} == data
     assert response.status_code == 404
