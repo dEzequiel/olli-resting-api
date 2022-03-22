beb9350902b8ba7925e241d3315c98348af767df
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-22 16:21:52 +0100

    feat: return a message to the user and status_code

--- controller/item_management.py
+++ controller/item_management.py
@@ -1,9 +1,13 @@
 from flask_restful import Resource
 from services.service import Services
 
+
 class ItemManagement(Resource):
 
     # 204: The server has successfully fulfilled the request and that there is
     # no additional content to send in the response payload body.
     def delete(self, item_id):
-        return 200 if Services.delete_item(item_id) else 404
+        if Services.delete_item(item_id):
+            return {f"ID {item_id}": "deleted"}, 200
+        else:
+            return {f"ID {item_id}": "not found"}, 404
