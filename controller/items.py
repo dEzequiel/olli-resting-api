07f4af030532bee646f20130facfa9ef91f06c1a
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-22 19:46:01 +0100

    feat: delete item by name conditionant

--- controller/items.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-
-from services.service import Services
-
-
-class Items(Resource):
-    def get(self):
-        return Services.get_items(), 200
--- repository/delete_item.py
+++ repository/delete_item.py
@@ -1,13 +1,13 @@
 from repository.database import get_db
 
 
-def delete_item(id):
+def delete_item(name):
 
     database = get_db()
 
 
-    if database.execute(f"SELECT * FROM inventory WHERE id='{id}'").fetchone():
-        database.execute(f"DELETE FROM inventory WHERE id='{id}'")
+    if database.execute(f"SELECT * FROM inventory WHERE name='{name}'").fetchone():
+        database.execute(f"DELETE FROM inventory WHERE name='{name}'")
         database.commit()
         database.close()
         return True #{f"ID {id}":"deleted"}
