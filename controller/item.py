5597b23cbebecb7900587bd8d19cb402ebdb9dfc
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-23 22:49:44 +0100

    feat: add request parser for post https method

--- controller/item.py
+++ controller/item.py
@@ -1,13 +1,20 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from services.service import Services
 
 
 class Item(Resource):
-    def get(self, name):
+    def get(self, name, sell_in, quality):
         return Services.get_item(name), 200
 
+
     def post(self, name):
-        return Services.post_item(name), 201
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True, location='json')
+        parser.add_argument('sell_in', type=int, required=True, location='json')
+        parser.add_argument('quality', type=int, required=True, location='json')
+        Services.post_item(name), 201
+        args = parser.parse_args()
+        return args
 
     # 204: The server has successfully fulfilled the request and that there is
     # no additional content to send in the response payload body.
