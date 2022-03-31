7467c3ff0b68363b984661a2ba14ebf4859bbf7b
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-31 16:25:08 +0200

    test: finish test to compare the value returned by service layer with the one returned by data access layer

--- tests/services/test_get_one_time.py
+++ tests/services/test_get_one_time.py
@@ -1,17 +1,11 @@
 from services.service import Services
 from repository.get_item import get_item
-from app import app
-import pytest
 
 services = Services()
 
 
-def test_get_one_item():
+def test_get_one_item(app):
     with app.app_context():
-
         # item make reference to the object returned by services layer
         # expected_result make reference to the object returned by data access layer
-
-        item = services.get_item("Sulfuras")
-        expected_result = get_item("Sulfuras")
-        assert expected_result == item
+        assert  services.get_item('Aged Brie') == get_item('Aged Brie')
