2cde79236c0bd4b818b9e8204c29c4b212f127ed
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-03-31 16:17:37 +0200

    test: add test to compare the value returned by database layer with the value returned by service layer

--- tests/services/test_get_all_items.py
+++ tests/services/test_get_all_items.py
@@ -1,17 +1,11 @@
 from services.service import Services
 from repository.get_item import get_all_items
-from app import app
 import pytest
 
 services = Services()
 
+# This test uses production database.
 
-def test_get_all_items():
+def test_get_all_items(app):
     with app.app_context():
-
-        # item make reference to the object returned by services layer
-        # expected_result make reference to the object returned by data access layer
-
-        item = services.get_items()
-        expected_result = get_all_items()
-        assert expected_result == item
+        assert services.get_all_items() == get_all_items()
